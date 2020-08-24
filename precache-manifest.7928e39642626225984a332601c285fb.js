08bad4031a3b28350a5daa709ae8990eb60458b9
Natsuki <nkondo3@gmail.com>
2020-08-24 18:26:02 +0900

    Updates

---
+++ precache-manifest.7928e39642626225984a332601c285fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea40b29279867133cb6e626a15161e2",
+    "url": "/todo-list-rct/index.html"
+  },
+  {
+    "revision": "ca964b933ebbfaaa8962",
+    "url": "/todo-list-rct/static/css/main.d489957a.chunk.css"
+  },
+  {
+    "revision": "ebf88246b064d712fef3",
+    "url": "/todo-list-rct/static/js/2.a86881ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-rct/static/js/2.a86881ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca964b933ebbfaaa8962",
+    "url": "/todo-list-rct/static/js/main.6302851c.chunk.js"
+  },
+  {
+    "revision": "ab54ac38541d2e80c180",
+    "url": "/todo-list-rct/static/js/runtime-main.6d3e6b30.js"
+  }
+]);
